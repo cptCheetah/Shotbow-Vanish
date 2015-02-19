f597d9dbbb469c4537db8cb1341b86dc7164f381
Miguel Angel <miguelangel.xispeta10@hotmail.com>
2015-02-19 15:17:31 +0100

    Create ParticleEffect.java

---
+++ src/com/topoedits/ParticleEffect.java
@@ -0,0 +1,18 @@
+package com.topoedits;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+    public class ParticleEffect {
+
+      public static Object WITCH_MAGIC;
+      public static Object HEART;
+      public static Object LAVA;
+
+
+      public void sendToPlayer(Player player, Location location, float offsetX, float offsetY, float offsetZ,
+                   float speed, int count) throws Exception {
+      }
+
+    }
